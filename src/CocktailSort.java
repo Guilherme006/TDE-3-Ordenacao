9c143ba71a9ef190394d6c2e4a2ed170f61d86c4
Guilherme006 <137656860+Guilherme006@users.noreply.github.com>
2025-10-24 17:32:32 -0300

    Update CocktailSort.java

--- src/CocktailSort.java
+++ src/CocktailSort.java
@@ -1,47 +1,52 @@
-public class CocktailSort implements Ordenador {
-
-    private void trocar(int[] vetor, int i, int j, Medidas medidas) {
-        int temporario = vetor[i];
-        vetor[i] = vetor[j];
-        vetor[j] = temporario;
-        medidas.trocas++;
-    }
-
-    @Override
-    public String obterNome() {
-        return "Cocktail Sort";
-    }
-
-    @Override
-    public Medidas ordenar(int[] vetor) {
+public class BucketSort {
+    public static Medidas ordenarPorFrequencia(int[] vetor, int tamanho) {
         Medidas medidas = new Medidas();
-        int inicio = 0;
-        int fim = vetor.length - 1;
-        boolean houveTroca = true;
 
-        while (houveTroca) {
-            houveTroca = false;
-            for (int i = inicio; i < fim; i++) {
-                medidas.interacoes++;
-                if (vetor[i] > vetor[i + 1]) {
-                    trocar(vetor, i, i + 1, medidas);
-                    houveTroca = true;
-                }
+        int minimo = vetor[0];
+        int maximo = vetor[0];
+        int i = 1;
+        while (i < tamanho) {
+            medidas.interacoes = medidas.interacoes + 1;
+            if (vetor[i] < minimo) {
+                minimo = vetor[i];
             }
-            if (!houveTroca) break;
-
-            houveTroca = false;
-            fim--;
-
-            for (int i = fim - 1; i >= inicio; i--) {
-                medidas.interacoes++;
-                if (vetor[i] > vetor[i + 1]) {
-                    trocar(vetor, i, i + 1, medidas);
-                    houveTroca = true;
-                }
+            medidas.interacoes = medidas.interacoes + 1;
+            if (vetor[i] > maximo) {
+                maximo = vetor[i];
             }
-            inicio++;
+            i = i + 1;
         }
+
+        int intervalo = maximo - minimo + 1;
+        if (intervalo < 1) intervalo = 1;
+
+        int[] frequencia = new int[intervalo];
+        i = 0;
+        while (i < intervalo) {
+            frequencia[i] = 0;
+            i = i + 1;
+        }
+
+        i = 0;
+        while (i < tamanho) {
+            int indice = vetor[i] - minimo;
+            frequencia[indice] = frequencia[indice] + 1;
+            i = i + 1;
+        }
+
+        int k = 0;
+        int valor = 0;
+        while (valor < intervalo) {
+            int cont = frequencia[valor];
+            while (cont > 0) {
+                vetor[k] = valor + minimo;
+                medidas.trocas = medidas.trocas + 1;
+                k = k + 1;
+                cont = cont - 1;
+            }
+            valor = valor + 1;
+        }
+
         return medidas;
     }
 }
